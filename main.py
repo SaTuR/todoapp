05da0ae48b5dc4130d0fb6519b0082d18273fb53
SaTuR <satur_cordova@hotmail.com>
2024-07-15 21:07:53 -0500

    ok

--- main.py
+++ main.py
@@ -9,25 +9,42 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tododb.sqlite"
 db = SQLAlchemy(app)
 # base de datos
 class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.Text, nullable=False)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    email = db.Column(db.Text, nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
     nombre = db.Column(db.Text, nullable=False)    
 
 class Tareas(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.Text, nullable=False)
-    estado = db.Column(db.Text, nullable=False)
+    estado = db.Column(db.Text, nullable=False,default='Pediente')
     email = db.Column(db.Text, nullable=False)
  
 with app.app_context():
     db.create_all()
+    # user = Usuario(
+    #         email='jscordovaa@gmail.com',
+    #         password='12345',nombre='Jose Cordova')
+    # db.session.add(user)
+    # db.session.commit()
 
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def index():
     if session:
-        return render_template('index.html',session=session)
+        if request.method=='POST':
+            name = request.form.get('name')
+            if name:
+                obj = Tareas(nombre=name,email=session['email'] )
+                db.session.add(obj)
+                db.session.commit()
+                lista_tareas = Tareas.query.all()
+            return render_template('index.html',lista_tareas = lista_tareas)
+
+        else:
+            lista_tareas = Tareas.query.all()
+            return render_template('index.html',session=session,lista_tareas = lista_tareas)
+            # return render_template('index.html',lista_tareas = lista_tareas)
     else:
         return redirect(url_for('login'))
 
@@ -42,13 +59,22 @@ def change_password():
 def add_task():
     pass
 
-@app.route("/update")
-def update_task():
-    pass
+@app.route("/update/<id>")
+def update_task(id):
+    obj = Tareas.query.filter_by(id = id).first()
+    if obj.estado == "Hecho":
+        obj.estado = "Pendiente"
+    else:
+        obj.estado = "Hecho"
+    db.session.commit()
+    return redirect(url_for('index'))
 
-@app.route("/delete")
-def delete_task():
-    pass
+@app.route("/delete/<id>")
+def delete_task(id):
+    obj= Tareas.query.filter_by(id=id).first()
+    db.session.delete(obj)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 #rutas No seguras 
 @app.route("/login", methods=['GET','POST'])
@@ -70,7 +96,10 @@ def logout():
 
 @app.route("/register")
 def register():
-    pass
+    obj = Usuario(email='jscordovaa@gmail.com',password="12345",nombre='Jose Cordova')
+    db.session.add(obj)
+    db.session.commit()
+    return redirect(url_for('login'))
 
 
 
